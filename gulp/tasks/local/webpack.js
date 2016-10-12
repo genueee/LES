21f1d50b91ef429b4c6540103dbd67210ba3c1dd
Volosojui <alessandro.kalinin@gmail.com>
2016-10-12 17:28:50 +0300

    JS Compress

--- gulp/tasks/local/webpack.js
+++ gulp/tasks/local/webpack.js
@@ -2,12 +2,23 @@
 var gulp = require('gulp'),
   gutil = require("gulp-util"),
   webpack = require('webpack'),
-  config = require('../../../webpack.config.scripts.local');
+  configUncompressed = require('../../../webpack.config.scripts.local'),
+  configCompressed = require('../../../webpack.config.scripts.local.min');
 
-gulp.task('[Local] Webpack', function(callback){
-  webpack(config, function(error, stats){
+gulp.task('[Local] WebpackUncompressed', function(callback){
+  webpack(configUncompressed, function(error, stats){
     if (error) throw new gutil.PluginError('webpack', error);
     gutil.log('[webpack]', stats.toString());
     callback();
   });
-})
+});
+
+gulp.task('[Local] WebpackCompressed', function(callback){
+  webpack(configCompressed, function(error, stats){
+    if (error) throw new gutil.PluginError('webpack', error);
+    gutil.log('[webpack]', stats.toString());
+    callback();
+  });
+});
+
+gulp.task('[Local] Webpack', ['[Local] WebpackUncompressed', '[Local] WebpackCompressed']);
